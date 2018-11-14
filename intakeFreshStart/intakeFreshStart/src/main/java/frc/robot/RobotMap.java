63afc5488fa8eaf4511940ddedf3facd3a38f516
Shadi Yadollahi Khales <JooJoo@Shadis-MacBook-Air.local>
2018-11-13 21:36:11 -0500

    Impliment Intake Take One

--- intakeFreshStart/intakeFreshStart/src/main/java/frc/robot/RobotMap.java
+++ intakeFreshStart/intakeFreshStart/src/main/java/frc/robot/RobotMap.java
@@ -18,7 +18,18 @@ public class RobotMap {
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
   // public static int rightMotor = 2;
+  public static int leftStick = 0;
+  public static int rightStick = 1;
+  public static int xboxController = 2;
 
+  public static class Intake {
+    public static int leftIntake = 0;
+    public static int rightIntake = 1;
+    public static int onSolenoid = 0;
+    public static int offSolenoid = 1;
+    public static int cubeDetector = 9;
+
+  }
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
---
+++ intakeFreshStart/intakeFreshStart/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+/**
+ * An example subsystem. You can replace me with your own Subsystem.
+ */
+public class Intake extends Subsystem {
+    Spark leftIntake = new Spark(RobotMap.Intake.leftIntake);
+    Spark rightIntake = new Spark(RobotMap.Intake.rightIntake);
+    Solenoid onSolenoid = new Solenoid(RobotMap.Intake.onSolenoid);
+    Solenoid offSolenoid = new Solenoid(RobotMap.Intake.offSolenoid);
+    DigitalInput cubeDetector = new DigitalInput(RobotMap.Intake.cubeDetector);
+
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public void IntakeOpen() {
+    onSolenoid.set(true);
+    offSolenoid.set(false);
+
+  }
+
+  public void IntakeClose() {
+    onSolenoid.set(false);
+    offSolenoid.set(true);
+
+  }
+  
+  public boolean IntakeIsOpen() {
+    return onSolenoid.get();
+
+  }
+
+  public boolean IntakeIsClose() {
+    return offSolenoid.get();
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
